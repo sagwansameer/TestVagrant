307399aef61f3ba2df56b5f9c7939a35602a8e47
Sameer Sagwan <sameer.sagwan1992@gmail.com>
2022-11-28 23:45:17 +0530

    Added util package, log4j and testNG.xml

---
+++ TestVagrant/src/main/java/com/build/rcb/base/UtilTest.java
@@ -0,0 +1,11 @@
+package com.build.rcb.base;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class UtilTest {
+	
+	Logger logger = LogManager.getLogger(UtilTest.class);
+
+}
--- TestVagrant/src/main/java/com/build/rcb/test/RoyalchallengersBangalore.java
+++ TestVagrant/src/main/java/com/build/rcb/test/RoyalchallengersBangalore.java
@@ -1,6 +1,31 @@
 package com.build.rcb.test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+
 public class RoyalchallengersBangalore {
 
-	
+	Logger logger = LogManager.getLogger(RoyalchallengersBangalore.class);
+
+	/*
+	 * @author Sameer Sagwan: This testcase validates that the team has only 4
+	 * foreign players
+	 */
+
+	@Test(testName = "test1_ForeignPlayers", description = "This testcase validates that the team has only 4 foreign players")
+
+	public void test1_ForeignPlayers() {
+		System.out.println("This is passed");
+
+	}
+
+	/*
+	 * @author Sameer Sagwan: This testcase validates that there is at least one
+	 * wicket keeper
+	 * 
+	 */
+	public void test2_WicketKeeper() {
+
+	}
 }
